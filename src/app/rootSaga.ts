8782108733f81c0d53f72dff3890e94eeb2b1337
manh <manhnt0210@gmail.com>
2022-07-12 22:42:46 +0700

    Add dashboard saga

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import counterSaga from "../features/counter/counterSaga";
 import authSaga from "../features/auth/authSaga";
+import dashboardSaga from '../features/dashboard/dashboardSaga';
 
 export default function* rootSaga() {
   yield all([
     counterSaga(),
     authSaga(),
+    dashboardSaga(),
   ])
 }
--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,6 +1,34 @@
-import * as React from 'react';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import {
+  dashboardActions,
+  selectDashboardLoading,
+  selectDashboardStatistics,
+  selectHighestStudentList,
+  selectLowestStudentList,
+  selectRankingByCityList
+} from './dashboardSlice';
 
 export default function Dashboard() {
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector(selectDashboardLoading);
+  const statistics = useAppSelector(selectDashboardStatistics);
+  const highestStudentList = useAppSelector(selectHighestStudentList);
+  const lowestStudentList = useAppSelector(selectLowestStudentList);
+  const RankingByCityList = useAppSelector(selectRankingByCityList);
+
+  console.log({
+    loading,
+    statistics,
+    highestStudentList,
+    lowestStudentList,
+    RankingByCityList
+  })
+
+  useEffect(() => {
+    dispatch(dashboardActions.fetchData());
+  }, [dispatch]);
+
   return (
     <div>
       Dashboard
